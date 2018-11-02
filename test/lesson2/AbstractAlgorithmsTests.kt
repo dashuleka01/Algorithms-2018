a91283067741cc0700ae4ce243a6f339540b439a
dashuleka01 <dashka01012000@mail.ru>
2018-11-02 23:58:08 +0300

    Commit #7 - sortTemperatures

--- test/lesson2/AbstractAlgorithmsTests.kt
+++ test/lesson2/AbstractAlgorithmsTests.kt
@@ -67,7 +67,7 @@ abstract class AbstractAlgorithmsTests {
         assertEquals(2, josephTask(2, 1))
         assertEquals(28, josephTask(40, 3))
         var menNumber = 2
-        for (i in 1..20) {
+        for (i in 1..19) {
             assertEquals(1, josephTask(menNumber, 2))
             menNumber *= 2
         }
